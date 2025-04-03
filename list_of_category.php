c3b154d9dae327f919c16d600123dbb9a849af23
Ayan Rahaman <ayanrahaman041@gmail.com>
2025-04-03 12:27:10 +0530

    mcommit

--- list_of_category.php
+++ list_of_category.php
@@ -1,6 +1,14 @@
 <?php
 //For connecting with the database
 require "connection.php";
+session_start();
+
+
+$user_first_name =  $_SESSION["user_first_name"];
+$user_last_name =  $_SESSION["user_last_name"];
+
+if(!empty($user_first_name) && !empty($user_last_name))
+{
 ?>
 
 <!DOCTYPE html>
@@ -35,3 +43,10 @@ require "connection.php";
     
 </body>
 </html>
+
+<?php
+}
+else{
+    header("location:login.php");
+}
+?>
