67928f3bc5f0e15c1f9a33817368635830d551e9
Ayan Rahaman <ayanrahaman041@gmail.com>
2025-04-04 15:31:05 +0530

    commit

--- add_category.php
+++ add_category.php
@@ -17,40 +17,66 @@ if(!empty($user_first_name) && !empty($user_last_name))
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Category</title>
+     <!-- font Awesome CDN -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- bootstrap CDN  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <?php
-    if(isset($_GET["category_name"]))
-    {
-        $category_name =  $_GET["category_name"];
-        $category_entrydate =  $_GET["category_entrydate"];
 
 
-        $sql = "INSERT INTO category (category_name,category_entrydate)
-        VALUES('$category_name','$category_entrydate')";
+<div class="container bg-light"> <!-- @Container Start -->
+            <div class="container-fluid border-bottom border-success"><!-- @Top Start -->
+                <?php include "topbar.php"; ?>
+            </div><!-- @End of Top -->
+            <div class="container-fluid">
+                <div class="row"> <!-- @Row Start-->
+                    <div class="col-sm-3 bg-light p-0 m-0"><!-- @Left Start-->
+                       <?php include "leftbar.php"; ?>
+                    </div> <!-- @End of Left -->
+                    <div class="col-sm-9 border-start border-success"> <!-- @Right Start -->
+                        <div class="container p-4 m-4"><!-- @Container Start-->
+                            <?php
+                            if(isset($_GET["category_name"]))
+                            {
+                                $category_name =  $_GET["category_name"];
+                                $category_entrydate =  $_GET["category_entrydate"];
 
 
-        if($conn->query($sql) == TRUE)
-        {
-            echo "data succesfully submitted";
-        }
-        else
-        {
-            echo "Data not inserted, something went wrong";
-        }
+                                $sql = "INSERT INTO category (category_name,category_entrydate)
+                                VALUES('$category_name','$category_entrydate')";
+
+
+                                if($conn->query($sql) == TRUE)
+                                {
+                                    echo "data succesfully submitted";
+                                }
+                                else
+                                {
+                                    echo "Data not inserted, something went wrong";
+                                }
 
 
 
-    }
+                            }
 
-    ?>
-    <form action="" method="GET">
-        Category : <br>
-        <input type="text" name="category_name"><br><br>
-        Category Entry Date : <br>
-        <input type="date" name="category_entrydate"><br><br>
-        <input type="submit" value="submit">
-    </form>
+                            ?>
+                            <form action="" method="GET">
+                                Category : <br>
+                                <input type="text" name="category_name"><br><br>
+                                Category Entry Date : <br>
+                                <input type="date" name="category_entrydate"><br><br>
+                                <input type="submit" value="submit" class="btn btn-success">
+                            </form>
+                            </div><!-- @End of Container-->
+                    </div><!-- @End of Right -->
+                </div><!-- @End of row -->
+            </div>
+            <div class="container-fluid border-top border-success"><!-- @Start of Bottom -->
+                <?php include "bottom.php"; ?>
+            </div><!-- @End of Bottom -->
+        </div><!-- @End of Container -->
+
 </body>
 </html>
 <?php
--- list_of_category.php
+++ list_of_category.php
@@ -17,29 +17,57 @@ if(!empty($user_first_name) && !empty($user_last_name))
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List of category</title>
+     <!-- font Awesome CDN -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- bootstrap CDN  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <?php
-    $sql = "SELECT * FROM category";
-    $query = $conn->query($sql);
-
-    echo "<table border='1'><tr><th>Category</th><th>Date</th><th>Action</th></tr>";
-    while($data = mysqli_fetch_assoc($query))
-    {
-        $category_id = $data["category_id"];
-        $category_name = $data["category_name"];
-        $category_entrydate = $data["category_entrydate"];
-
-        echo "<tr>
-                <td>$category_name</td>
-                <td>$category_entrydate</td>
-                <td><a href='edit_category.php?id=$category_id'>Edit</a></td>
-            </tr>";
-    }
-    echo "</tr></table>"
 
 
-    ?>
+<div class="container bg-light"> <!-- @Container Start -->
+            <div class="container-fluid border-bottom border-success"><!-- @Top Start -->
+                <?php include "topbar.php"; ?>
+            </div><!-- @End of Top -->
+            <div class="container-fluid">
+                <div class="row"> <!-- @Row Start-->
+                    <div class="col-sm-3 bg-light p-0 m-0"><!-- @Left Start-->
+                       <?php include "leftbar.php"; ?>
+                    </div> <!-- @End of Left -->
+                    <div class="col-sm-9 border-start border-success"> <!-- @Right Start -->
+                        <div class="container p-4 m-4"><!-- @Conteiner Start -->
+                            <?php
+                                    $sql = "SELECT * FROM category";
+                                    $query = $conn->query($sql);
+
+                                    echo "<table class='table table-success table-striped table-hover'><tr><th>Category</th><th>Date</th><th>Action</th></tr>";
+                                    while($data = mysqli_fetch_assoc($query))
+                                    {
+                                        $category_id = $data["category_id"];
+                                        $category_name = $data["category_name"];
+                                        $category_entrydate = $data["category_entrydate"];
+
+                                        echo "<tr>
+                                                <td>$category_name</td>
+                                                <td>$category_entrydate</td>
+                                                <td>
+                                                <a href='edit_category.php?id=$category_id' class='btn btn-success'>Edit</a>
+                                                </td>
+                                            </tr>";
+                                    }
+                                    echo "</tr></table>"
+                            ?>
+                        </div><!-- @End of Container -->
+                    </div><!-- @End of Right -->
+                </div><!-- @End of row -->
+            </div>
+            <div class="container-fluid border-top border-success"><!-- @Start of Bottom -->
+                <?php include "bottom.php"; ?>
+            </div><!-- @End of Bottom -->
+        </div><!-- @End of Container -->
+
+
+    
     
 </body>
 </html>
